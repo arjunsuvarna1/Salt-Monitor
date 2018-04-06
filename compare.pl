d387064b20b571d38fa36510fecd08cafd46a53c
Arjun <arjunsuvarna.s@gmail.com>
2018-04-06 22:25:54 +0530

    perl file writes to report.txt

--- compare.pl
+++ compare.pl
@@ -8,5 +8,12 @@ chomp( my @a_file = read_file 'file_1.txt' );
 chomp( my @b_file = read_file 'file_2.txt' );
 
 my @a_file_only = List::Compare->new( \@a_file, \@b_file )->get_Lonly;
+open my $fh, '>', "report.txt" or die "Cannot open output.txt: $!";
 
-print "$_\n" for @a_file_only;
+# Loop over the array
+foreach (@a_file_only)
+{
+    print $fh "$_\n"; # Print each entry in our array to the file
+}
+close $fh;
+#print "$_\n" for @a_file_only;
